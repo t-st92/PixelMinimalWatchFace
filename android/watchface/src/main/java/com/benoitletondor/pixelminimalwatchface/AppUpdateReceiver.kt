9cf1241f23454fb8bb43f5e28b8bcabb6cc00bfd
Benoit Letondor <benoit.letondor@gmail.com>
2021-10-06 01:57:20 +0200

    Update month of release

--- android/watchface/src/main/java/com/benoitletondor/pixelminimalwatchface/AppUpdateReceiver.kt
+++ android/watchface/src/main/java/com/benoitletondor/pixelminimalwatchface/AppUpdateReceiver.kt
@@ -46,7 +46,7 @@ class AppUpdateReceiver : BroadcastReceiver() {
 
     @Suppress("SameParameterValue", "UNUSED_PARAMETER")
     private fun onAppUpgrade(context: Context, oldVersion: Int, newVersion: Int) {
-        if( oldVersion < 64 ) {
+        if( oldVersion < 65 ) {
             val storage = Injection.storage(context)
 
             if( !storage.hasFeatureDropSummer2021NotificationBeenShown() ) {
