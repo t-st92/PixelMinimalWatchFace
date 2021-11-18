a8486521dd810692a9a91bd83d8bc5c086092b48
Benoit Letondor <benoit.letondor@gmail.com>
2021-11-18 23:43:26 +0100

    Bump version and activate logs

--- android/watchface/src/main/java/com/benoitletondor/pixelminimalwatchface/PixelMinimalWatchFace.kt
+++ android/watchface/src/main/java/com/benoitletondor/pixelminimalwatchface/PixelMinimalWatchFace.kt
@@ -62,7 +62,7 @@ private const val DATA_KEY_BATTERY_STATUS_PERCENT = "/batterySync/batteryStatus"
 private const val THREE_DAYS_MS: Long = 1000 * 60 * 60 * 24 * 3
 private const val THIRTY_MINS_MS: Long = 1000 * 60 * 30
 private const val MINIMUM_COMPLICATION_UPDATE_INTERVAL_MS = 1000L
-val DEBUG_LOGS = BuildConfig.DEBUG
+val DEBUG_LOGS = true
 private const val TAG = "PixelMinimalWatchFace"
 
 class PixelMinimalWatchFace : CanvasWatchFaceService() {
