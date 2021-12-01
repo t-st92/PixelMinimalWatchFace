1cebe3f306048e73e083f0ec6cb5c51fa5b9a84f
Benoit Letondor <benoit.letondor@gmail.com>
2021-12-01 11:00:44 +0100

    Enhance stale algo

--- android/watchface/src/main/java/com/benoitletondor/pixelminimalwatchface/WatchBatteryStatus.kt
+++ android/watchface/src/main/java/com/benoitletondor/pixelminimalwatchface/WatchBatteryStatus.kt
@@ -6,11 +6,19 @@ sealed class WatchBatteryStatus {
     object Unknown : WatchBatteryStatus()
     data class DataReceived(
         val batteryPercentage: Int,
-        val timestamp: Long,
     ) : WatchBatteryStatus() {
-        fun isStale(currentBatteryPercentage: Int): Boolean {
+        private var staleSinceTs: Long? = null
+
+        fun markAsStale() {
+            if (staleSinceTs == null) {
+                staleSinceTs = System.currentTimeMillis()
+            }
+        }
+
+        fun shouldRefresh(currentBatteryPercentage: Int): Boolean {
             val diff = abs(batteryPercentage - currentBatteryPercentage)
-            return diff > 1 || System.currentTimeMillis() - timestamp > ONE_MIN_MS
+            val staleSinceTs = this.staleSinceTs
+            return diff > 1 || (staleSinceTs != null && System.currentTimeMillis() - staleSinceTs > ONE_MIN_MS)
         }
 
         companion object {
